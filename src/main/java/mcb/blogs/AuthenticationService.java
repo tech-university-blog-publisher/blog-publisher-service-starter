808566180a41f2d3ee25ec359dc3d3b03c2884bf
Mahesh Haldar <mahesh_haldar@mckinsey.com>
2018-06-05 08:54:26 +0400

    Add JWT

--- src/main/java/mcb/blogs/AuthenticationService.java
+++ src/main/java/mcb/blogs/AuthenticationService.java
@@ -1,19 +1,25 @@
 package mcb.blogs;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/authenticate")
 @Scope("request")
 public class AuthenticationService {
     private BlogUser user;
+    final String SECRET_KEY = "some-secret";
 
     @Autowired
     public AuthenticationService(BlogUser user) {
@@ -21,8 +27,33 @@ public class AuthenticationService {
     }
 
     @PostMapping
-    public ResponseEntity authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity authenticate(@RequestBody AuthenticationRequest request) throws Exception {
         // generate JWT
-        return ResponseEntity.ok(request);
+
+        return ResponseEntity.ok(generateJWT(request.getUsername()));
+    }
+
+    public String generateJWT(String username) throws Exception {
+
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            String token = JWT.create()
+                    .withIssuer("auth0")
+                    .withSubject(username)
+                    .withExpiresAt(new Date(2342342342344L))
+                    .sign(algorithm);
+            return token;
+    }
+
+    @RequestMapping(value = "/validate", method = RequestMethod.POST)
+    public ResponseEntity validate(@RequestBody ValidateTokenRequest request) throws Exception {
+
+        String token = request.getToken();
+        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+        JWTVerifier jwtVerifier = JWT.require(algorithm)
+                .withIssuer("auth0")
+                .build();
+        DecodedJWT decodedJWT = jwtVerifier.verify(token);
+        return ResponseEntity.ok(decodedJWT);
+
     }
 }
---
+++ src/main/java/mcb/blogs/ValidateTokenRequest.java
@@ -0,0 +1,13 @@
+package mcb.blogs;
+
+public class ValidateTokenRequest {
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    String token;
+
+}
